32f2b41d5b50341228e3b603654389cf8e710764
Yuqing Wu <ywu20@stuy.edu>
2022-05-27 09:12:37 -0400

    merge changes with react

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello():
-        return "Hello, welcome to P4!"
+    return "Hello, welcome to P4!"
+
+@app.route("/canvas")
+def canvas():
+    return render_template('canvas.html')
 if __name__ == "__main__":
-        app.run(host='0.0.0.0')
+    app.run()
