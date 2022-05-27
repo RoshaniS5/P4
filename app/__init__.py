26df81f98e3b0981ff910fa7c86455ffcc02f020
Roshani Shrestha <roshanishrestha983@gmail.com>
2022-05-27 09:21:12 -0400

    update py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,53 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, welcome to P4!"
+# Sleepy Starfish - Roshani Shrestha (Pete), Yuqing Wu (Zero), Angela Zhang (Inky), Hebe Huang (Umbreon)
+# SoftDev Pd2
+# P04 -- Project Iris
+# 2022-05-27
 
-@app.route("/canvas")
-def canvas():
-    return render_template('canvas.html')
+from flask import Flask, render_template, request, session, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+        '''Displays the home page.'''
+        return render_template('index.html')
+
+@app.route("/time")
+def sortByTime():
+        '''Sorts notes by time, from most recent to least recent.'''
+        return render_template('index.html')
+
+@app.route("/name")
+def sortByName():
+        '''Sorts notes in alphabetical order by name.'''
+        return render_template('index.html')
+
+@app.route("/search")
+def search():
+        '''Displays all notes with the specified name as the recipient'''
+        return render_template('index.html')
+
+@app.route("/about")
+def about():
+        '''Displays the about page.'''
+        return render_template('about.html')
+
+@app.route("/notes")
+def notes():
+        '''Displays the notes page, which allows the user to submit a note.'''
+        return render_template('canvas.html')
+
+@app.route("/send")
+def send():
+        '''Add submission.'''
+        return redirect("/")
+
+@app.route("/note")
+def note():
+        '''Displays the note page, which allows the user to view an individual note.'''
+        return render_template('note.html')
+
 if __name__ == "__main__":
-    app.run()
+        app.debug = True
+        app.run()
