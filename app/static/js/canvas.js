2eb4f33d1cd6cdfce5eb0f9c49d7a5e4b3256ad3
Yuqing Wu <ywu20@stuy.edu>
2022-06-01 08:50:58 -0400

    canvas

--- app/static/js/canvas.js
+++ app/static/js/canvas.js
@@ -1,4 +1,25 @@
-// Sleepy Starfish | Roshani Shrestha, Yuqing Wu, Angela Zhang, Hebe Huang
-// SoftDev Pd2
-// P04 -- Project Iris | Home Page
-// 2022-05-24
+// /** @jsx React.DOM */
+//
+// // Sleepy Starfish | Roshani Shrestha, Yuqing Wu, Angela Zhang, Hebe Huang
+// // SoftDev Pd2
+// // P04 -- Project Iris | Home Page
+// // 2022-05-24
+// //var a = React.createClass({
+//   render: function(){
+// console.log("hello");
+// var c = document.getElementById("can");
+// console.log(c);
+// var ctx = c.getContext('2d');
+// ctx.fillRect();
+// return "fhjksdhfka";
+// }
+// })
+// module.exports = a;
+function blah(){
+  console.log("hdskjfshkfh")
+  c = document.getElementById("can");
+  console.log(c);
+  ctx=c.getContext("2d");
+  ctx.fillStyle ="blue"
+  ctx.fillRect(100,100,100,100);
+}
